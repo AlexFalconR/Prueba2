ab3dfcbc651c541f17faa952983c8dcfddd42e19
AlexFalconR <alex.falcon.25@hotmail.com>
2015-04-06 01:29:40 -0300

    Signed-off-by: AlexFalconR <alex.falcon.25@hotmail.com>

---
+++ Prueba2.0/src/Prueba2.0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	int a,b,c;
+	cout<< "ingrese numeros a sumar";
+	cin>> a;
+	cout<< "ingrese numeros a sumar";
+	cin>> b;
+	c=(a + b);
+	cout <<"el resultado es,"<< c ;
+	return 0;
+}
